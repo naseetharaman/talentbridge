6cd946a42b92a8a2dea543f009be2c48789947e7
Jowhar, Mohammed(mjowhar) <mjowhar@paypal.com>
2016-12-23 18:45:11 +0530

    adding code

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 'use strict';
 
-var express = require('express');
-var kraken = require('kraken-js');
+/* eslint no-console: 0 */
+
+var express = require('express'),
+    kraken = require('kraken-js'),
+   
+    http = require('http');
+    
+
+var db = require('./lib/database');
+var configPassport = require('./lib/passport-setup');
 
 
 var options, app;
@@ -16,8 +24,19 @@ options = {
          * Add any additional config setup or overrides here. `config` is an initialized
          * `confit` (https://github.com/krakenjs/confit/) configuration object.
          */
+        var mountpath = config.get('express:mountpath');
+        app.get('/favicon.ico', function (req, res) {
+            res.redirect(mountpath + req.path);
+        });
+        //TODO: move the db config to config.json file 
+        var dbConfig = {
+            'host' : 'localhost',
+            'database' : 'talentbridge'
+        };
+        db.config(dbConfig);
+        configPassport.setupPassport(app);
         next(null, config);
-    }
+    },
 };
 
 app = module.exports = express();
@@ -25,4 +44,16 @@ app.use(kraken(options));
 app.on('start', function () {
     console.log('Application ready to serve requests.');
     console.log('Environment: %s', app.kraken.get('env:env'));
+});
+
+var server;
+
+/*
+ * Create and start HTTP server.
+ */
+
+server = http.createServer(app);
+server.listen(process.env.PORT || 8000);
+server.on('listening', function () {
+    console.log('Server listening on http://localhost:%d', this.address().port);
 });
